0bf8e5296d6eb8feb4d240fd7a2a7537fef59393
Benjamin Adams <adams.benjamin@protonmail.com>
2023-05-05 11:50:27 -0400

    d1p2

--- 2022/src/day01.rs
+++ 2022/src/day01.rs
@@ -6,9 +6,13 @@ const FILE_PATH: &str = "./inputs/day01";
 fn main() -> io::Result<()> {
 
     let content = fs::read_to_string(FILE_PATH).expect("Couldn't read file.'");
-    let packs = parse_packs(Vec::from_iter(content.split("\n").into_iter()));
+    let mut packs = parse_packs(Vec::from_iter(content.split("\n").into_iter()));
 
-    println!("The elf is carrying {:?} calories.", packs.iter().max().unwrap());
+    packs.sort();
+    packs.reverse();
+    packs.truncate(3);
+
+    println!("{:?}", packs.iter().sum::<u32>());
 
     Ok(())
 }
